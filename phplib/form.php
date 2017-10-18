2eb2b1fc84077de7bc6f641aaca00581500c1bbb
krittaphat <32318713+krittaphat@users.noreply.github.com>
2017-10-18 19:19:18 +0700

    Initial commit

---
+++ phplib/form.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Form control: input</h2>
+  <p>The form below contains two input elements; one of type text and one of type password:</p>
+  <form>
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" class="form-control" id="name">
+    </div>
+    <div class="form-group">
+      <label for="lname">lastname:</label>
+      <input type="text" class="form-control" id="lname">
+    </div>
+    <div class="form-group">
+      <label for="ct">Contact:</label>
+      <input type="text" class="form-control" id="ct">
+    </div>
+    <div class="form-group">
+      <label for="ac">active:</label>
+      <input type="num" class="form-control" id="ac">
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
---
+++ phplib/retive.php
@@ -0,0 +1,23 @@
+<?php
+  $servername = "127.0.0.1";
+  $username = "root";
+  $password = "";
+  $db = "cruddatabase";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password,$db);
+
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  //echo "Connected successfully";
+  $sql = "Select * from members";
+  $result = $conn->query($sql);
+  //var_dump($result);
+  while ($row = mysqli_fetch_assoc($result)) {
+    //var_dump($row);
+    echo $row['id']. " : " . $row['fname']. " : " . $row['lname'] . " : " .$row['contact']. "<br>";
+  }
+  mysqli_close ($conn);
+?>
